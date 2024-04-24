43d76cc07b2da3dd4bce63d97d0ec583198bf473
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-04-24 22:38:30 +0530

    Update index.js

--- components/FiltersGroup/index.js
+++ components/FiltersGroup/index.js
@@ -1,11 +1,58 @@
 import {IoSearchOutline} from 'react-icons/io5'
 import './index.css'
 
+const SelectCategoryOptions = props => {
+  const {id, name, getCategoryId} = props
+  const onClickedCategory = () => {
+    getCategoryId(id)
+  }
+  return (
+    <button type="button" className="btn-style" onClick={onClickedCategory}>
+      <p className="category-para">{name}</p>
+    </button>
+  )
+}
+
+const SelectRatings = props => {
+  const {ratingId, imageUrl, getRatingId} = props
+  const onClickedRating = () => {
+    getRatingId(ratingId)
+  }
+  const imageAlt = 'rating{ratingId}'
+  return (
+    <button
+      type="button"
+      className="rating-btn-style"
+      onClick={onClickedRating}
+    >
+      <img src={imageUrl} alt={imageAlt} className="rating-image" />
+      <p className="rating-up">& up</p>
+    </button>
+  )
+}
+
 const FiltersGroup = props => {
-  const {changeTitleSearch, title} = props
+  const {
+    changeTitleSearch,
+    title,
+    categoryOptions,
+    changeCategorySearch,
+    ratingsList,
+    changeRating,
+    clearAllFilters,
+  } = props
   const onChangeTitleSearch = event => {
     changeTitleSearch(event.target.value)
     console.log(event.target.value)
+  }
+  const getCategoryId = id => {
+    changeCategorySearch(id)
+  }
+  const getRatingId = id => {
+    changeRating(id)
+  }
+  const onClickClearFilters = () => {
+    clearAllFilters()
   }
   return (
     <div className="filters-group-container">
@@ -19,6 +66,37 @@ const FiltersGroup = props => {
         />
         <IoSearchOutline />
       </div>
+      <div className="select-label-container">
+        <h1 className="select-category-label">Category</h1>
+        {categoryOptions.map(cat => (
+          <SelectCategoryOptions
+            key={cat.categoryId}
+            id={cat.categoryId}
+            name={cat.name}
+            getCategoryId={getCategoryId}
+          />
+        ))}
+      </div>
+      <div className="select-label-container">
+        <label htmlFor="category" className="select-category-label">
+          Rating
+        </label>
+        {ratingsList.map(rat => (
+          <SelectRatings
+            key={rat.ratingId}
+            ratingId={rat.ratingId}
+            imageUrl={rat.imageUrl}
+            getRatingId={getRatingId}
+          />
+        ))}
+      </div>
+      <button
+        type="button"
+        className="clear-filters-btn"
+        onClick={onClickClearFilters}
+      >
+        Clear Filters
+      </button>
     </div>
   )
 }
